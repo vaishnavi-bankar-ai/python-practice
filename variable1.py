5ee9d3a34c65fece72a46589d64b5673231fd4c3
Vaishnavi Bankar <vaishnavi.bankar199@gmail.com>
2026-09-06 18:11:16 +0530

    practice of int and float variable types

--- variable1.py
+++ variable1.py
@@ -6,3 +6,22 @@ x=3.9*x*(1+x)
 print(x) 
 name = "variable topic: very simple example"
 print(name)
+i=42
+print(i)
+print(type(i))
+f=30.5
+print(f)
+print(type(f))
+f= int(80.0)
+print(f)
+print(type(f))
+i= float(50)
+print(i)
+print(type(i))
+print(float(80)+100)
+print(int(70)+100)
+print(int(40)+100.0)
+print(int(30)+float(25))
+
+
+
