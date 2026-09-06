b2d436474a12c0f884989fcb7c4f8cd809683d3e
Vaishnavi Bankar <vaishnavi.bankar199@gmail.com>
2026-09-06 22:13:53 +0530

    string conversion topic simple practice

---
+++ stringconversion.py
@@ -0,0 +1,23 @@
+sval='1234'
+type(sval)
+print(type(sval))
+print(sval)
+tval=int(sval)
+type(tval)
+print(type(tval))
+print(tval+2)
+abc="12"
+xyz="88"
+print(type(abc))
+print(type(xyz))
+print ("abc=" , abc)
+print("xyz=" ,xyz)
+print("abc+xyz=" , abc+xyz)
+print("abc+xyz=" , abc + xyz)
+n=int(abc)
+m=float(xyz)
+print ("n=" , n)
+print ("m=" , m)
+print("n+m=" , n+m)
+done="string conversion concept is clear"
+print(done)
