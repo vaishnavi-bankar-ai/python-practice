1a03ae5f11088ff91434b25d66e8a08211b122d1
Vaishnavi Bankar <vaishnavi.bankar199@gmail.com>
2026-09-06 23:04:18 +0530

    Elevator floor number  convert program

---
+++ elevatorconvert.py
@@ -0,0 +1,6 @@
+# in most part of the world including Europe, ground floor of the hotel is called as Zero floor but in US,ground floor of the hotel is called as first floor
+# write a program to convert Europe elevator floor no into US elevator floor no
+euf=input("Europe floor?")
+usf= int(euf)+1
+print("US floor is " , usf)
+print("Europe elevator floor no= ", int(euf), "means US elevator no=" , usf )
