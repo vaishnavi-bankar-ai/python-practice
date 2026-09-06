63069f3782694ff081eda1b51e67bf46cb8da21d
Vaishnavi Bankar <vaishnavi.bankar199@gmail.com>
2026-09-06 23:30:48 +0530

    program for basic pay calculator

---
+++ calculator.py
@@ -0,0 +1,5 @@
+# write a basic program for pay calculator
+hrs= input("What are the no of hours?")
+rate=input("What is the rate per hour")
+grosspay= float(hrs) * float(rate)
+print("Pay:" , grosspay)
