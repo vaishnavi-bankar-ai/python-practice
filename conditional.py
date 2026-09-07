d5277fead81765865c59cd74a0ec99d19c0256ff
Vaishnavi Bankar <vaishnavi.bankar199@gmail.com>
2026-09-07 19:50:30 +0530

    Conditional statement:indenting and deindenting topic practice

--- conditional.py
+++ conditional.py
@@ -25,3 +25,15 @@ if x==6:
     print("x is still 6")
     print("Third 6")
 print("Afterwords 6")
+# Conditional Statement: indenting and deindenting topic
+x=5
+if x>2:
+    print("Bigger than 2")
+    print("Still Bigger")
+print("Done with 2")
+for i in range(5):
+    print(i)
+    if i>2:
+        print("Bigger than 2")
+    print("Done with i",i)
+print("All Done")
