da8accfd9b919bd806dda5526c8d94f131683b6e
Vaishnavi Bankar <vaishnavi.bankar199@gmail.com>
2026-09-07 18:47:26 +0530

    conditional statements basic practice

---
+++ conditional.py
@@ -0,0 +1,13 @@
+x = 5
+if x == 5:
+    print('x Equals 5')
+if x >= 5:
+    print('x is greater than or equals 5')
+if x > 4:
+    print("x is greater than 4")
+if x <= 5:
+    print("x is less than or equals 5")
+if x<6:
+    print("x is less than 6")
+if x!=6:
+    print("x is not equal to 6")                   
