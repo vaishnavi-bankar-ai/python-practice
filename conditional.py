b88dd5a300d0b3d4c8ba66e7b8e2f07ede9ca4c6
Vaishnavi Bankar <vaishnavi.bankar199@gmail.com>
2026-09-07 19:40:23 +0530

    Conditional statements:One-Way Decisions

--- conditional.py
+++ conditional.py
@@ -10,4 +10,18 @@ if x <= 5:
 if x<6:
     print("x is less than 6")
 if x!=6:
-    print("x is not equal to 6")                   
+    print("x is not equal to 6") 
+#Conditional Statement: One-Way decisions topic code practice
+x=5
+print("Before 5")
+if x==5:
+    print("x is 5")   
+    print("x is still 5")
+    print("Third 5")
+print("Afterwords 5")
+print("Before 6")
+if x==6:
+    print("x is 6")
+    print("x is still 6")
+    print("Third 6")
+print("Afterwords 6")
