e6bc1d3960edc8aa00c662513a7f24fa89675db3
Vaishnavi Bankar <vaishnavi.bankar199@gmail.com>
2026-09-08 09:43:01 +0530

    functions:min function basic code

--- functions.py
+++ functions.py
@@ -7,6 +7,14 @@ thing()
 print("function's very basic code")
 thing()
 # max function: built-in function ; appearently lowercase letters are bigger than uppercase letters
-#max function : basic practice code
+# max function : basic practice code
 big = max("Hello world")
 print(big)
+large = max("VirtualStudiocodecv")
+print(large)
+# min function: built-in function: appearently space is the smallest thing
+# min function: basic practice code
+tiny = min("Hello World")
+print(tiny)
+small = min("virtualStudioCodecc")
+print(small)
