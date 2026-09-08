9dde005f4a1565c32e60a414d41c1f5c355b4426
Vaishnavi Bankar <vaishnavi.bankar199@gmail.com>
2026-09-08 16:35:27 +0530

    functions: return value rules and multiple parameters/arguments code practice

--- function_return.py
+++ function_return.py
@@ -8,3 +8,10 @@ def greet(name):
 print(greet("mr"), "who are you?")
 print(greet("mi"), "how are you?")
 print(greet("jn"), "where are you")
+#functions: multiple parameters/arguments topic code
+def addtwo(a,b):
+    print(a)
+    return a+b
+    print(b) # when python reaches return,the function ends immediately and hence print(b) will never execute in this function
+x = addtwo(10,20)
+print(x)
