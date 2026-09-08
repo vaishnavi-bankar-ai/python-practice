1ea16376e5d1b301e1bf1799c717d3fb987d59dd
Vaishnavi Bankar <vaishnavi.bankar199@gmail.com>
2026-09-08 09:28:49 +0530

    Functions: very basic practice code

---
+++ functions.py
@@ -0,0 +1,8 @@
+# def is a keyword stands for the definition of a function,define the function
+# def is a keyword and it ends with :
+def thing():
+    print("Hello")
+    print("Fun")
+thing()
+print("function's very basic code")
+thing()
