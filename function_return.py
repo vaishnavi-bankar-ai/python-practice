838bbb96a92364200dfb3db2407382d2264a7cc6
Vaishnavi Bankar <vaishnavi.bankar199@gmail.com>
2026-09-08 16:26:15 +0530

    functions:return value example code

---
+++ function_return.py
@@ -0,0 +1,10 @@
+def greet(name):
+    if name == "jn":
+        return "John"
+    elif name == "mi":
+        return "Michael"
+    else :
+        return "hey stranger"
+print(greet("mr"), "who are you?")
+print(greet("mi"), "how are you?")
+print(greet("jn"), "where are you")
