b7fb293b941308485c0135ee468075781b0a83b6
Vaishnavi Bankar <vaishnavi.bankar199@gmail.com>
2026-09-09 08:44:25 +0530

    while loop:zero tip loop example

--- while.py
+++ while.py
@@ -4,3 +4,11 @@ while n>2:
     n=n-1
 print("Blastoff!")
 print("n=",n)
+#Zero trip loop:loop never executed because its never enter in a loop
+# zero trip loop : example code
+n=0
+while n>0:
+    print("hello")
+    print("Fun")
+print("Ohh Sorry")
+
