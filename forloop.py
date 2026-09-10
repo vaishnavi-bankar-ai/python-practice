9a1d122768518198643c948aea58ceefdaff52d5
Vaishnavi Bankar <vaishnavi.bankar199@gmail.com>
2026-09-10 22:53:07 +0530

    for loop :for loop with strings example

--- forloop.py
+++ forloop.py
@@ -1,4 +1,12 @@
 for i in (5,4,3,2,1):
     print(i)
 print("blastoff!")
+# for loop/definite loop with strings:
+friends = ["John","Mohan","Michael"]
+for friend in friends :
+    print("Happy New Year",friend)
+    print("God bless you",friend)
+print("Done!")
+
+
 
