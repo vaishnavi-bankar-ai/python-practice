b807c00b755cf111cadb8cc40f07bd295425e4c6
Vaishnavi Bankar <vaishnavi.bankar199@gmail.com>
2026-09-12 20:31:05 +0530

    Find the smallest value

--- numvalue.py
+++ numvalue.py
@@ -5,4 +5,12 @@ for the_num in (9,41,12,3,74,15):
         largest_so_far= the_num
     print(largest_so_far, the_num)
 print("after" ,largest_so_far)
+#find smallest value :example code
+smallest_so_far = -1
+print("before",smallest_so_far)
+for the_num in (9,41,12,3,74,15):
+    if the_num < smallest_so_far:
+        smallest_so_far = the_num
+    print(smallest_so_far,the_num)
+print("after",smallest_so_far)
 
