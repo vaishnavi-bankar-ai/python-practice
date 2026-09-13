8a9084f79e5330881fb585b17fad60172a68fcbb
Vaishnavi Bankar <vaishnavi.bankar199@gmail.com>
2026-09-13 15:18:23 +0530

    loop:summing/total in a loop

--- countloop.py
+++ countloop.py
@@ -4,3 +4,10 @@ for thing in [3,41,12,9,74,15]:
     count= count + 1
     print(count,thing)
 print("after", count)
+# summing/totaling in a loop 
+sum=0
+print("before",sum)
+for thing in[9,41,12,3,74,15]:
+    sum= sum+thing
+    print(sum,thing)
+print("after",sum)
