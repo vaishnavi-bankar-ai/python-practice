aa3621e4c147cf09e37c915061b78459013d1cc8
Vaishnavi Bankar <vaishnavi.bankar199@gmail.com>
2026-09-13 16:48:56 +0530

    find a smallest value in a list

--- numvalue.py
+++ numvalue.py
@@ -1,16 +1,17 @@
 largest_so_far=-1
 print("before",largest_so_far)
-for the_num in (9,41,12,3,74,15):
+for the_num in [9,41,12,3,74,15]:
     if the_num > largest_so_far:
         largest_so_far= the_num
     print(largest_so_far, the_num)
 print("after" ,largest_so_far)
 #find smallest value :example code
-smallest_so_far = -1
-print("before",smallest_so_far)
-for the_num in (9,41,12,3,74,15):
-    if the_num < smallest_so_far:
-        smallest_so_far = the_num
-    print(smallest_so_far,the_num)
-print("after",smallest_so_far)
-
+smallest=None
+print("before")
+for value in [9,41,12,3,74,15]:
+    if smallest is None:
+        smallest=value
+    elif value<smallest:
+        smallest=value
+    print(smallest,value)
+print("After",smallest)
