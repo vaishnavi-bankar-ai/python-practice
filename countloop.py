1a91eb5195d33e9149a4958a2b407cab4b4dcccf
Vaishnavi Bankar <vaishnavi.bankar199@gmail.com>
2026-09-13 15:28:35 +0530

    loop:finding average in a loop

--- countloop.py
+++ countloop.py
@@ -11,3 +11,13 @@ for thing in[9,41,12,3,74,15]:
     sum= sum+thing
     print(sum,thing)
 print("after",sum)
+#Finding average in a loop
+count = 0
+sum = 0
+print("before","count:", count,"sum:", sum)
+for value in [9,41,12,3,74,15]:
+    count=count+1
+    sum=sum+value
+    print(count,sum,value)
+print("after","count:",count,"sum:",sum,"Average:",sum/count)
+
