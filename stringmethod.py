28e2229118db60234545f1178d091d4571070916
Vaishnavi Bankar <vaishnavi.bankar199@gmail.com>
2026-09-21 21:26:54 +0530

    String method: find function useful for finding a position/place of any character in a string

--- stringmethod.py
+++ stringmethod.py
@@ -3,3 +3,9 @@ print(len(course))
 # string method: string specific functions. they are represented by dot operator
 print(course.upper())
 print(course.lower())
+#string method:find function: to find a position/place of a character/characters in a string
+print(course.find('P'))
+print(course.find('o'))
+print(course.find('O'))
+print(course.find('beginners'))
+print(course.find('or'))
